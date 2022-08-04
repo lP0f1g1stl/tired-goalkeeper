a326bf3b77d7aebe3c7ad6d2b8921251c4399a8f
lP0f1g1stl <64088803+lP0f1g1stl@users.noreply.github.com>
2022-08-04 11:14:25 +0300

    up

---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private Button[] _restartButtons;
+
+    private void OnEnable()
+    {
+        for(int i = 0; i < _restartButtons.Length; i++) 
+        {
+            _restartButtons[i].onClick.AddListener(ReloadScene);
+        }
+    }
+    private void OnDisable()
+    {
+        for (int i = 0; i < _restartButtons.Length; i++)
+        {
+            _restartButtons[i].onClick.RemoveListener(ReloadScene);
+        }
+    }
+    private void ReloadScene() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
